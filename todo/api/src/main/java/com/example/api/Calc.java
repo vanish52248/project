2368663723f89097c81decf9ae55a3fd9d4b218e
vanish52248 <vanish52248@gmail.com>
2023-03-19 00:29:00 +0900

    UT追加

---
+++ todo/api/src/main/java/com/example/api/Calc.java
@@ -0,0 +1,12 @@
+package com.example.api;
+
+public class Calc {
+
+    int multiplication(int x, int y) {
+        return x * y;
+    }
+
+    int zeroDivisionError(int x, int y) {
+        return x / y;
+    }
+}
