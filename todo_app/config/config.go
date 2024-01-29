9db935a315a662b9e2f495d4b021a2a3f6178919
vanish52248 <vanish52248@gmail.com>
2024-01-30 00:32:40 +0900

    TODO config_log 設定

---
+++ todo_app/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"sandbox/project/todo_app/utils"
+
+	"gopkg.in/go-ini/ini.v1"
+)
+
+type ConfigList struct {
+	Port      string
+	SQLDriver string
+	DbName    string
+	LogFile   string
+}
+
+var Config ConfigList
+
+func init() {
+	LoadConfig()
+	utils.LoggingSettings(Config.LogFile)
+
+	// 文字列に変数埋め込みテンプレートリテラル
+	file := "config.ini"
+	temp := fmt.Sprintf("%s is loaded!\n", file)
+	fmt.Println(temp)
+}
+
+func LoadConfig() {
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	Config = ConfigList{
+		Port:      cfg.Section("web").Key("port").MustString("8080"),
+		SQLDriver: cfg.Section("db").Key("driver").String(),
+		DbName:    cfg.Section("db").Key("name").String(),
+		LogFile:   cfg.Section("web").Key("logfile").String(),
+	}
+}
---
+++ todo_app/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sandbox/project/todo_app/config"
+)
+
+func main() {
+	fmt.Println(config.Config.Port)
+	fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)
+	fmt.Println(config.Config.LogFile)
+
+	log.Println("test")
+}
